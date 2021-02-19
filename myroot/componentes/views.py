ef0464be598f85e4ab9c747083e8a67179f02366
RodrigOssaV <49349061+RodrigOssaV@users.noreply.github.com>
2021-02-19 13:31:58 -0300

    permissions-auth
    decorators

--- myroot/componentes/views.py
+++ myroot/componentes/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .decorators import unauthenticated_user
 
 # Create your views here.
+@login_required(login_url='login')
 def home(request):
     return render(request, 'main.html', {})
 
+@unauthenticated_user
 def mainPage(request):
     return render(request, 'welcome.html', {})

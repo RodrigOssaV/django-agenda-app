6edfcbc9d28e0056ebdf05f54a91e68fb6ac92e3
RodrigOssaV <49349061+RodrigOssaV@users.noreply.github.com>
2021-02-18 21:23:31 -0300

    fix-navbar
    fix navbar
    fix welcome page
    fix navbar user

--- myroot/componentes/urls.py
+++ myroot/componentes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+#localhost:8000/
 urlpatterns = [
-    path('', views.home, name = 'home'),
+    #path('', views.home, name = 'home'),
+    path('', views.mainPage, name = 'mainPage')
 ]
